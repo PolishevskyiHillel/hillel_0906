676819db4c8f9f31c1f4c357019670957bde5da3
PolishevskyiHillel <141448896+PolishevskyiHillel@users.noreply.github.com>
2023-09-17 19:29:05 +0300

    Lesson 27 Only Page Object

---
+++ src/test/java/lesson27/PageObjectExample.java
@@ -0,0 +1,23 @@
+package lesson27;
+
+import init.InitialDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import ua.page.object.MainPageLogic;
+import ua.page.object.SearchPageLogic;
+
+public class PageObjectExample extends InitialDriver {
+
+    @Test
+    public void testWithPageObject() {
+        driver.get("https://rozetka.com.ua/ua/");
+        String tittleFirstProductFromSearchPage = new MainPageLogic(driver)
+                .typeTextToInputSearch("Mac")
+                .clickSearchBtn()
+                .getFirstProductTittle();
+        String tittleOfProductOnProductPage = new SearchPageLogic(driver)
+                .clickOnFirstProduct()
+                .getTittleOfProduct();
+        Assert.assertEquals(tittleFirstProductFromSearchPage, tittleOfProductOnProductPage);
+    }
+}
